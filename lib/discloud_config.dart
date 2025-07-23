4500995365d9e9a60b4ed0d130538714bac76fa0
Gorniaky <hwss.s2@gmail.com>
2025-07-23 17:18:43 -0300

    feat: implement file listing utility with error handling

--- lib/discloud_config.dart
+++ lib/discloud_config.dart
@@ -4,4 +4,5 @@ export 'src/constants.dart';
 export 'src/data.dart';
 export 'src/discloud_config.dart';
 export 'src/scopes.dart';
+export 'src/utils/file_list_search.dart';
 export 'src/utils/list_discloud_config.dart';
---
+++ lib/src/utils/file_list_search.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+/// Recursively lists files in a directory and its subdirectories,
+/// filtering them based on a test function.
+///
+/// The [where] function is called for each file found, and only files for
+/// which the function returns `true` are included in the output stream.
+///
+/// The [onError] function is called when an error occurs while listing the
+/// files. If it is not provided, errors are ignored.
+Stream<File> fileListSearch(
+  Directory directory,
+  bool Function(File file) where, [
+  Function? onError,
+]) async* {
+  onError ??= (_) => null;
+
+  await for (final e in directory.list(recursive: true).handleError(onError)) {
+    if (e is File && where(e)) yield e;
+  }
+}
