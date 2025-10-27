0893efa191749f25d71ff021c04b3e0a5de4f710
Gorniaky <hwss.s2@gmail.com>
2025-10-27 12:46:13 -0300

    fix export of DiscloudConfigData extension

--- lib/discloud_config.dart
+++ lib/discloud_config.dart
@@ -3,6 +3,7 @@ library;
 export "src/constants.dart";
 export "src/data.dart";
 export "src/discloud_config.dart";
+export "src/extensions/data.dart";
 export "src/scopes.dart";
 export "src/utils/file_list_search.dart";
 export "src/utils/list_discloud_config.dart";
